59e6f238b67eac51bed21d16e3ae920be473a12e
Nguyen Dang Duc Linh <duclinhqn1510@gmail.com>
2018-12-19 18:20:34 +0700

    handle error connect reset

--- neotx/utils.go
+++ neotx/utils.go
@@ -1,156 +1,14 @@
-package main
+package neotx
 
 import (
-	"log"
-	// "io"
-	"strings"
-	"os"
-	"time"
-	"github.com/o3labs/neo-transaction-watcher/neotx"
-	"github.com/o3labs/neo-transaction-watcher/neotx/network"
-	"github.com/streadway/amqp"
 	"encoding/json"
 	"net"
 	"net/http"
 	"sort"
+	"strings"
 	"sync"
+	"time"
 )
-
-type Handler struct {
-}
-var connectedToNEONode = false
-var hasError = false
-var wg = new(sync.WaitGroup) 
-//implement the message protocol
-func (h *Handler) OnReceive(tx neotx.TX) {
-	log.Printf("%+v", tx)
-	 if tx.Type.String() == "block" || tx.Type.String() == "tx" {
-		log.Printf(" msg: %+v", tx.ID)
-		go sendMsg("{ \"Type\":"+ "\"" + tx.Type.String() + "\","+ "\"ID\":" +"\"" + tx.ID +  "\"}") 
-	 }
-}
-
-func (h *Handler) OnConnected(c network.Version) {
-	log.Printf("connected %+v", c)
-	connectedToNEONode = true
-	hasError = false 
-}
-
-func (h *Handler) OnError(e error) {
-	log.Printf("error %+v", e)
-	// if e == io.EOF && connectedToNEONode == true {
-	if connectedToNEONode == true {
-		connectedToNEONode = false
-		log.Printf("Disconnected from host. will try to connect in 5 seconds...")
-		// for {
-			
-			time.Sleep(5 * time.Second)
-			//we need to implement backoff and retry to reconnect here
-			//if the error is EOF then we try to reconnect
-			// wg.Add(1)
-			// wg.Done() 
-			
-			hasError = true
-		}
-	// }
-}
-
-func startConnectToSeed() {
-	const  PRIV_MAGIC = 56753
-	const  TESTNET_MAGIC = 1953787457
-	const  MAINET_MAGIC = 7630401
-	const  TEST_PORT = 20333
-	const  PRIV_PORT = 8333
-	const MAIN_PORT = 10333
-	const PRIV_ADDRESS = "118.27.35.112"
-	// const TEST_ADDRESS = "118.27.35.112"
-	const TEST_ADDRESS = "test1.cityofzion.io"
-	const MAIN_ADDRESS =  "seed9.ngd.network"
-
-	// var nodes = []string{"http://seed8.ngd.network:10332", "http://seed6.ngd.network:10332", "http://seed3.ngd.network:10332"}
-	// var a = getBestNode(nodes)
-	// log.Printf("getBestNode %+v", a)
-
-	config := neotx.Config{
-		Network:   network.NEONetworkMagic(MAINET_MAGIC),
-		Port:      MAIN_PORT,
-		// IPAddress: "seed4.neo.org",
-		IPAddress: MAIN_ADDRESS,
-	}
-	client := neotx.NewClient(config)
-	handler := &Handler{}
-	client.SetDelegate(handler)
-
-	err := client.Start()
-	if err != nil {
-		log.Printf("%v", err)
-		os.Exit(-1)
-	}
-
-	for {
-		if hasError {
-			startConnectToSeed()
-			hasError = false
-			return
-		}
-	}
-}
-
-func main() {
-	// wg.Add(1)
-	startConnectToSeed()
-	// wg.Wait()
-	// startConnectToSeed()
-}
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
-	}
-}
-
-func sendMsg(msg string) {
-	log.Printf("%s", msg)	
-	const QUEUE_NEO_NOTIFICATION = "queue_neo_notification"
-	conn, err := amqp.Dial("amqp://test:test@127.0.0.1:5762")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-	
-	q, err := ch.QueueDeclare(
-		QUEUE_NEO_NOTIFICATION, // name
-		true,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
- 
-	body :=  msg
-	err = ch.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		})
-	log.Printf(" [x] Sent %s", body)
-	failOnError(err, "Failed to publish a message")
-}
-
-func getBestNode(list []string) *SeedNodeResponse {
-	commaSeparated := strings.Join(list, ",")
-	return SelectBestSeedNode(commaSeparated)
-}
-
-
-
-//******************************************** utils *****************************************************************************8
 
 type customTransport struct {
 	rtp       http.RoundTripper
@@ -216,19 +74,15 @@ func fetchSeedNode(url string) *BlockCountResponse {
 	//instead of using default http client. we use a transport one here.
 	//because we need to mearure the time. Request, Response and total duration.
 	//to select the best node among the nodes that has the highest blockcount by picking the least latency node.
-	log.Printf("  url %v", url)
 	transport := newTransport()
 	client := http.Client{Transport: transport}
-	payload := strings.NewReader(" {\"jsonrpc\": \"2.0\", \"method\": \"getblockcount\", \"params\": [], \"id\": 1}")
+	payload := strings.NewReader(" {\"jsonrpc\": \"2.0\", \"method\": \"getblockcount\", \"params\": [], \"id\": 3}")
 	res, err := client.Post(url, "application/json", payload)
-	log.Printf("  err %v", err)
 	if err != nil || res == nil {
 		return nil
 	}
 	defer res.Body.Close()
 	blockResponse := BlockCountResponse{}
-	log.Printf("  blockResponse %v", blockResponse)
-	
 	err = json.NewDecoder(res.Body).Decode(&blockResponse)
 	if err != nil {
 		return nil
@@ -302,7 +156,6 @@ loop:
 	sort.SliceStable(listHighestNodes, func(i, j int) bool {
 		return listHighestNodes[i].BlockCount > listHighestNodes[j].BlockCount
 	})
-	log.Printf("  listHighestNodes %v", len(listHighestNodes))
 	if len(listHighestNodes) == 0 {
 		return nil
 	}
